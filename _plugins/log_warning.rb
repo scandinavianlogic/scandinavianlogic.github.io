4b27ba94ecbdb017e61447d5f87f13bb0fb49e33
Anders Lundstedt <git@anderslundstedt.se>
2019-03-20 15:08:26 +0100

    added a plugin with a filter used to log warnigns

---
+++ _plugins/log_warning.rb
@@ -0,0 +1,13 @@
+# Modifed the example from https://talk.jekyllrb.com/t/how-to-properly-indicate-an-error-during-site-generation/447/2
+
+module Jekyll
+  module WarningFilter
+    def log_warning(msg)
+        bad_file = @context.registers[:page]['path']
+        err_msg = "\033[31mOn #{bad_file}: #{msg}\033[0m"
+        warn err_msg
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::WarningFilter)
