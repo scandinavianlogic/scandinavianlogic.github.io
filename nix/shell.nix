0538e8eb5ad4dd1cc1559bf6033f3bcd13ca6718
Anders Lundstedt <git@anderslundstedt.se>
2020-10-19 15:21:37 +0200

    update shell.nix

--- nix/shell.nix
+++ nix/shell.nix
@@ -3,11 +3,16 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "env";
 
+  # solution from
+  # https://stackoverflow.com/questions/37933375/how-to-build-a-ruby-gem-using-nix-that-has-native-extensions
+  shellHook = lib.optional stdenv.isDarwin ''
+    export XML2_LIB=${stdenv.lib.makeLibraryPath [ libxml2 ]}
+    bundle config build.nokogiri --use-system-libraries --with-xml2-lib=$XML2_LIB
+  '';
+
   buildInputs = [
     libxslt
     ruby
-    bashInteractive
-    zlib
     pkgconfig
     libxml2
     bundler
