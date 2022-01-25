2085d997ce2c20f5f10e6ff5755c3e8fd9fd3063
Anders Lundstedt <git@anderslundstedt.se>
2022-01-26 00:35:15 +0100

    ‘stdenv.lib’ ↦ ‘lib’

--- nix/shell.nix
+++ nix/shell.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation {
   # solution from
   # https://stackoverflow.com/questions/37933375/how-to-build-a-ruby-gem-using-nix-that-has-native-extensions
   shellHook = lib.optional stdenv.isDarwin ''
-    export XML2_LIB=${stdenv.lib.makeLibraryPath [ libxml2 ]}
+    export XML2_LIB=${lib.makeLibraryPath [ libxml2 ]}
     bundle config build.nokogiri --use-system-libraries --with-xml2-lib=$XML2_LIB
   '';
 
