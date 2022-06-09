22fcc56b0ff11527536f0b07555c8cd29bda94ab
Anders Lundstedt <git@anderslundstedt.se>
2022-06-09 02:46:31 +0200

    rename ‘nix’ ↦ ‘anders_files’

---
+++ anders_files/make_symlinks.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+cd $(dirname $0)
+cd ../.git/info
+rm exclude
+ln -s ../../anders_files/exclude .
+cd ..
+cd ..
+ln -sf anders_files/envrc .envrc
+ln -sf anders_files/shell.nix .
+ln -sf anders_files/Makefile .
