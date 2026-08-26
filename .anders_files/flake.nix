6f8f69da5c6a8d1cff29672bbb8dbb1bcd8368ea
Anders Lundstedt <git@anderslundstedt.se>
2026-08-26 11:43:59 +0200

    sync flake inputs with my nix-pins repo

--- .anders_files/flake.nix
+++ .anders_files/flake.nix
@@ -1,9 +1,9 @@
 {
   description = "A GitHub Pages development environment";
 
-  inputs.pins.url        = "github:anderslundstedt/nix-pins";
-  inputs.nixpkgs.follows = "pins/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.pins.url            = "github:anderslundstedt/nix-pins";
+  inputs.nixpkgs.follows     = "pins/nixpkgs-unstable";
+  inputs.flake-utils.follows = "pins/flake-utils";
 
   outputs = {self,nixpkgs,flake-utils,...}:
     flake-utils.lib.eachDefaultSystem (system:
